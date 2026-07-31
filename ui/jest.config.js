deb52ec2f0d89c76a15e85a4dc10996b64a25885
halng <haonguyentan2001@gmail.com>
2026-07-31 21:48:10 +0700

    fix(ci): enable integration  test

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'jest-expo',
   
-  // 1. Tell Jest to compile these specific packages (Standard Expo list)
+  // Transform RN/Expo under pnpm's virtual store (.pnpm/...) as well as hoisted layout.
+  // See https://docs.expo.dev/develop/unit-testing/#pnpm
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    'node_modules/(?!(.pnpm|(jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native/js-polyfills))',
   ],
 
   // 2. Help Jest understand your alias "@/"
